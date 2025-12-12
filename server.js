703c412487c26510f46b04147d8412732374bab7
Gavin Hardison <ghardison120@gmail.com>
2025-12-11 21:41:28 -0800

    added 55 Cancri e to main page and added product page

--- server.js
+++ server.js
@@ -233,6 +233,15 @@ app.get('/products/wasp12b', (req, res) => {
     })
 })
 
+app.get('/products/55cancrie', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'products', '55cancrie.html'), (err) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send('An error has occurred')
+        }
+    })
+})
+
 // send planets JSON
 app.get('/planets', (req, res) => {
     res.json(starSystems)
